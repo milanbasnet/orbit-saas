720c9b13fa87afdad60def7725f0d12134552f22
milanbasnet <millnbasnet544@gmail.com>
2026-05-26 14:00:14 +0545

    ci yml file added

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { AUTH_EXPIRED_EVENT } from "../services/http";
 import {
